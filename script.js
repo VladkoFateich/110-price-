38b0904366ea5c9b5f4d7acf39151cf069c4fcd6
VladkoFateich <92536769+VladkoFateich@users.noreply.github.com>
2023-07-13 15:27:58 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,93 +1,45 @@
-let modalCola = document.getElementById("infoModalCola");
-let btnCola = document.getElementById("buttonModalCola");
-let closeCola = document.getElementById("closeCola");
-btnCola.onclick = function () {
-  modalCola.style.display = "block";
-};
-closeCola.onclick = function () {
-  modalCola.style.display = "none";
-};
+window.addEventListener("DOMContentLoaded", () => {
+  const infoBtn = document.querySelectorAll(".button"),
+    modal = document.querySelectorAll(".modal"),
+    closeBtn = document.querySelectorAll(".close"),
+    parent = document.querySelector(".imgConteiner");
 
-window.onclick = function (event) {
-  if (event.target === modalCola) {
-    modalCola.style.display = "none";
+  function openModal(i) {
+    modal[i].classList.toggle("show");
+    document.body.style.overflow = "hidden";
   }
-};
-
-let modalZero = document.getElementById("infoModalZero");
-let btnZero = document.getElementById("buttonModalZero");
-let closeZero = document.getElementById("closeZero");
-btnZero.onclick = function () {
-  modalZero.style.display = "block";
-};
-closeZero.onclick = function () {
-  modalZero.style.display = "none";
-};
-
-window.onclick = function (event) {
-  if (event.target === modalZero) {
-    modalZero.style.display = "none";
+  function closeModal(j) {
+    modal[j].classList.remove("show");
+    document.body.style.overflow = "";
   }
-};
+  parent.addEventListener("click", (event) => {
+    if (event.target && event.target.classList.contains("button")) {
+      infoBtn.forEach((item, i) => {
+        if (event.target == item) {
+          openModal(i);
+        }
+      });
+    }
+    if (event.target && event.target.classList.contains("close")) {
+      closeBtn.forEach((item, j) => {
+        if (event.target == item) {
+          closeModal(j);
+        }
+      });
+    }
+  });
 
-let modalFancy = document.getElementById("infoModalFancy");
-let btnFancy = document.getElementById("buttonModalFancy");
-let closeFancy = document.getElementById("closeFancy");
-btnFancy.onclick = function () {
-  modalFancy.style.display = "block";
-};
-closeFancy.onclick = function () {
-  modalFancy.style.display = "none";
-};
-
-window.onclick = function (event) {
-  if (event.target === modalFancy) {
-    modalFancy.style.display = "none";
-  }
-};
-
-let modalStreet = document.getElementById("infoModalStreet");
-let btnStreet = document.getElementById("buttonModalStreet");
-let closeStreet = document.getElementById("closeStreet");
-btnStreet.onclick = function () {
-  modalStreet.style.display = "block";
-};
-closeStreet.onclick = function () {
-  modalStreet.style.display = "none";
-};
-
-window.onclick = function (event) {
-  if (event.target === modalStreet) {
-    modalStreet.style.display = "none";
-  }
-};
-let modalOchakovsky = document.getElementById("infoModalOchakovsky");
-let btnOchakovsky = document.getElementById("buttonModalOchakovsky");
-let closeOchakovsky = document.getElementById("closeOchakovsky");
-btnOchakovsky.onclick = function () {
-  modalOchakovsky.style.display = "block";
-};
-closeOchakovsky.onclick = function () {
-  modalOchakovsky.style.display = "none";
-};
-
-window.onclick = function (event) {
-  if (event.target === modalStreet) {
-    modalOchakovsky.style.display = "none";
-  }
-};
-let modalSemSekret = document.getElementById("infoModalSemSekret");
-let btnSemSekret = document.getElementById("buttonModalSemSekret");
-let closeSemSekret = document.getElementById("closeSemSekret");
-btnSemSekret.onclick = function () {
-  modalSemSekret.style.display = "block";
-};
-closeSemSekret.onclick = function () {
-  modalSemSekret.style.display = "none";
-};
-
-window.onclick = function (event) {
-  if (event.target === modalStreet) {
-    modalSemSekret.style.display = "none";
-  }
-};
+  closeBtn.forEach((item) => {
+    item.addEventListener("click", closeModal);
+  });
+  document.addEventListener("keydown", (e) => {
+    if (e.code === "Escape" && modal.classList.contains("show")) {
+      closeModal();
+    }
+  });
+  modal.addEventListener("click", (e) => {
+    if (e.target === modal) {
+      closeModal();
+    }
+  });
+});
